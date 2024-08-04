f6f29cdad6470d7b9e40f17cb91a9ac737b773de
Dustin Yang <dustinyang@DustinucBookPro.lan>
2024-08-03 20:36:33 -0400

    update

---
+++ OpenAIClient/OpenAIClient.h
@@ -0,0 +1,18 @@
+//
+//  OpenAIClient.h
+//  OpenAIClient
+//
+//  Created by Dustin Yang on 8/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OpenAIClient.
+FOUNDATION_EXPORT double OpenAIClientVersionNumber;
+
+//! Project version string for OpenAIClient.
+FOUNDATION_EXPORT const unsigned char OpenAIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenAIClient/PublicHeader.h>
+
+
---
+++ Text2ImageApp/Text2ImageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text2ImageAppApp.swift
+//  Text2ImageApp
+//
+//  Created by Dustin Yang on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Text2ImageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
